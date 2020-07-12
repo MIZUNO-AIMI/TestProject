57108ce9b6378ec0f42408ca8fc1584f45693723
a_mizuno <a_mizuno@freestyles.jp>
2020-07-12 23:33:13 +0900

    [2020/07/11] @16h DBの表示処理実装

--- NameBattler/app/src/main/java/com/example/namebattler/CharacterList.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CharacterList.java
@@ -4,25 +4,90 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
 
 public class CharacterList extends AppCompatActivity {
 
+    ListView myListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_character_list);
-
         // データを渡す為のBundleを生成し、渡すデータを内包させる
         Bundle bundle = new Bundle();
         bundle.putString("TITLE", "キャラ一覧( )人");
 
+        //listview
+        myListView = (ListView)findViewById(R.id.myListView);
+
         //ボタンを設定
         Button button3 = this.findViewById(R.id.button3);
+
+        setContentView(R.layout.activity_character_list);
+        // DB作成
+        CustomOpenHelper helper = new CustomOpenHelper(getApplicationContext());
+        ArrayAdapter<String> arrayAdapter;
+
+        //読み取り専用で良い場合はgetReadableDatabase()を呼び出す
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
+        arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        // ListViewにArrayAdapterを設定する
+        myListView.setAdapter(arrayAdapter);
+
+        try{
+        Cursor cursor = db.query(
+                "DBName", // DB名
+                new String[]{"COLUMN_NAME_TITLE1", "COLUMN_NAME_TITLE2"
+                        , "COLUMN_NAME_TITLE3", "COLUMN_NAME_TITLE4"
+                        , "COLUMN_NAME_TITLE5", "COLUMN_NAME_TITLE6"
+                        , "COLUMN_NAME_TITLE7", "COLUMN_NAME_TITLE8"
+                        , "COLUMN_NAME_TITLE9"
+                },
+                // 取得するカラム名
+                null, // WHERE句の列名
+                null, // WHERE句の値
+                null, // GROUP BY句の値
+                null, // HAVING句の値
+                null);// ORDER BY句の値
+
+
+            StringBuilder sbuilder = new StringBuilder();
+
+            for (int i = 0; i < cursor.getCount(); i++) {
+                sbuilder.append(cursor.getString(0));
+                sbuilder.append("      ");
+                sbuilder.append(cursor.getInt(1));
+                sbuilder.append("\nHP:");
+                sbuilder.append(cursor.getInt(2));
+                sbuilder.append(" MP:");
+                sbuilder.append(cursor.getInt(3));
+                sbuilder.append(" STR:");
+                sbuilder.append(cursor.getInt(4));
+                sbuilder.append(" DEF:");
+                sbuilder.append(cursor.getInt(5));
+                sbuilder.append(" AGI:");
+                sbuilder.append(cursor.getInt(6));
+
+                cursor.moveToFirst();
+            }
+
+            cursor.close();
+        }
+        finally{
+            db.close();
+        }
+
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +113,6 @@ public class CharacterList extends AppCompatActivity {
         transaction.add(R.id.container, fragment);
         // 最後にcommitを使用することで変更を反映します
         transaction.commit();
-
 
     }
 
--- NameBattler/app/src/main/java/com/example/namebattler/CustomOpenHelper.java
+++ NameBattler/app/src/main/java/com/example/namebattler/CustomOpenHelper.java
@@ -7,13 +7,39 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class CustomOpenHelper extends SQLiteOpenHelper {
-    public CustomOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
-    // データベース自体の名前(テーブル名ではない)
-    static final private String DBName = "NAMEBATTLER_DB";
+
+    // データベースの名前
+    static final private String DBName = "NameBattler";
+    private static final String TABLE_NAME = "CHARACTERS";
+    private static final String _ID = "_id";
+    private static final String COLUMN_NAME_TITLE1 = "NAME";
+    private static final String COLUMN_NAME_TITLE2 = "JOB";
+    private static final String COLUMN_NAME_TITLE3 = "HP";
+    private static final String COLUMN_NAME_TITLE4 = "MP";
+    private static final String COLUMN_NAME_TITLE5 = "STR";
+    private static final String COLUMN_NAME_TITLE6 = "DEF";
+    private static final String COLUMN_NAME_TITLE7 = "AGI";
+    private static final String COLUMN_NAME_TITLE8 = "LUCK";
+    private static final String COLUMN_NAME_TITLE9 = "CREATE_AT";
+
     // データベースのバージョン(2,3と挙げていくとonUpgradeメソッドが実行される)
     static final private int VERSION = 1;
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    _ID + " INTEGER PRIMARY KEY," +
+                    COLUMN_NAME_TITLE1 + " TEXT," +
+                    COLUMN_NAME_TITLE2 + " INTEGER," +
+                    COLUMN_NAME_TITLE3 + " INTEGER," +
+                    COLUMN_NAME_TITLE4 + " INTEGER," +
+                    COLUMN_NAME_TITLE5 + " INTEGER," +
+                    COLUMN_NAME_TITLE6 + " INTEGER," +
+                    COLUMN_NAME_TITLE7 + " INTEGER," +
+                    COLUMN_NAME_TITLE8 + " INTEGER," +
+                    COLUMN_NAME_TITLE9 + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + TABLE_NAME;
 
     // コンストラクタ　以下のように呼ぶこと
     public CustomOpenHelper(Context context){
@@ -23,13 +49,20 @@ public class CustomOpenHelper extends SQLiteOpenHelper {
     // データベースが作成された時に実行される処理
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // 処理を記述
+        // テーブル作成
+        db.execSQL(
+                SQL_CREATE_ENTRIES
+        );
     }
 
     // データベースをバージョンアップした時に実行される処理
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // 処理を記述
+        // アップデートの判別、古いバージョンは削除して新規作成
+        db.execSQL(
+                SQL_DELETE_ENTRIES
+        );
+        onCreate(db);
     }
 
     // データベースが開かれた時に実行される処理
